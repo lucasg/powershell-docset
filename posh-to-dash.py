997e8af012c68d4c88b7da7db4b53e063d7ff6be
lucasg <lucas.georges@outlook.com>
2017-08-14 00:13:41 +0200

    Initial commit with MVP for Powershell docset
    Currently only list Modules and Cmdlets.
    Paths are broken and there is no themes
    Docset icon though

---
+++ posh-to-dash.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+
+import sqlite3, os, urllib, subprocess, hashlib
+import os
+import shutil
+import logging
+import json
+import tarfile
+import urllib.parse
+
+from bs4 import BeautifulSoup as bs # pip install bs4
+import requests
+
+
+# CONFIGURATION
+posh_doc_api_version = '0.2' # powershell doc api version, not this docset one.
+posh_version = '6'
+docset_name = 'Powershell'
+
+default_url = "https://docs.microsoft.com/en-us/powershell/module/?view=powershell-%s"
+# default_toc = "https://docs.microsoft.com/api/apibrowser/powershell/modules?moniker=powershell-%s&api-version=%s"
+default_toc = "https://docs.microsoft.com/en-us/powershell/module/powershell-%s/toc.json?view=powershell-%s"
+
+def download_binary(url, output_filename):
+
+    r = requests.get(url, stream=True)
+    with open(output_filename, 'wb') as f:
+        for data in r.iter_content(32*1024):
+            f.write(data)
+
+def download_textfile(url, output_filename):
+
+    r = requests.get(url)
+    with open(output_filename, 'w', encoding="utf8") as f:
+        f.write(r.text)
+
+def crawl_posh_documentation(documents_folder, posh_version = posh_version):
+
+    index = default_url % posh_version
+    modules_toc = default_toc % (posh_version, posh_version)
+
+    index_filepath = os.path.join(documents_folder, "index.html")
+    download_textfile(index, index_filepath)
+
+    modules_filepath = os.path.join(documents_folder, "modules.toc")
+    download_textfile(modules_toc, modules_filepath)
+
+    modules_filepath = os.path.join(documents_folder, "modules.toc")
+    with open(modules_filepath, 'r') as modules_fd:
+        modules = json.load(modules_fd)
+
+        for module in modules['items'][0]['children']:
+            module_url = urllib.parse.urljoin(modules_toc, module["href"])
+
+            module_dir = os.path.join(documents_folder, module['toc_title'])
+            os.makedirs(module_dir, exist_ok = True)
+
+            r = requests.get(module_url)
+            module_filepath = os.path.join(module_dir, "index.html")
+            download_textfile(module_url, module_filepath)
+            
+
+            for cmdlet in module['children']:
+                cmdlet_name = cmdlet['toc_title']
+                
+                if cmdlet_name == "About" or cmdlet_name == "Providers": # skip special toc
+                    continue
+
+                cmdlet_urlpath = cmdlet["href"]
+                cmdlet_url = urllib.parse.urljoin(modules_toc, cmdlet_urlpath)
+
+                cmdlet_filepath = os.path.join(module_dir, "%s.html" % cmdlet_name)
+                download_textfile(cmdlet_url, cmdlet_filepath)
+            
+
+
+
+def update_db(db, cur, name, typ, path):
+    try:
+        cur.execute('SELECT rowid FROM searchIndex WHERE path = ?', (path,))
+        dbpath = cur.fetchone()
+        cur.execute('SELECT rowid FROM searchIndex WHERE name = ?', (name,))
+        dbname = cur.fetchone()
+
+        if dbpath is None and dbname is None:
+            cur.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (name, typ, path))
+            logging.debug('DB add >> name: %s, path: %s' % (name, path))
+        else:
+            logging.debug('record exists')
+
+    except:
+        pass
+
+
+def make_docset(source_dir, dst_dir, filename):
+    """ https://stackoverflow.com/a/17081026/1741450 """
+    
+    tar_filepath = os.path.join(dst_dir, '%s.tar' % filename)
+    targz_filepath = os.path.join(dst_dir, '%s.tar.gz' % filename)
+    docset_filepath = os.path.join(dst_dir, '%s.docset' % filename)
+
+    with tarfile.open(tar_filepath, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
+    shutil.move(tar_filepath, targz_filepath)
+    shutil.copy(targz_filepath, docset_filepath)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+
+    destination_dir = os.getcwd()
+
+    # Docset archive format
+    """ 
+        $root/
+            $docset_name.docset/
+                Contents/
+                    Info.plist
+                    Resources/
+                        LICENSE
+                        docSet.dsidx
+                        Documents/
+                            *
+    """
+    docset_dir = os.path.join(destination_dir, "%s.docset" % docset_name)
+    content_dir = os.path.join(docset_dir , "Contents")
+    resources_dir = os.path.join(content_dir, "Resources")
+    document_dir = os.path.join(resources_dir, "Documents")
+    os.makedirs(document_dir, exist_ok=True)
+
+    shutil.copy("Info.plist", content_dir)
+    shutil.copy("LICENSE", resources_dir)
+
+
+    # Crawl and download powershell modules documentation
+    # crawl_posh_documentation(document_dir)
+
+    # Download icon for package
+    # download_binary("https://github.com/PowerShell/PowerShell/raw/master/assets/Powershell_16.png", os.path.join(docset_dir, "icon.png"))
+
+    # Create database and index html doc
+    sqlite_filepath = os.path.join(resources_dir, "docSet.dsidx")
+    os.remove(sqlite_filepath)
+    db = sqlite3.connect(sqlite_filepath)
+    cur = db.cursor()
+    cur.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+    cur.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+
+    modules = filter(lambda x : os.path.isdir(x), map(lambda y: os.path.join(document_dir, y), os.listdir(document_dir)))
+    for module in modules:
+        module_name = os.path.basename(module)
+        update_db(db, cur, module_name, "Module", "Contents/Resources/Documents/%s/index.html" % module_name)
+
+        for f in filter(lambda x : os.path.isfile(os.path.join(document_dir, module_name, x)), os.listdir(module)):
+            
+            cmdlet_filename = os.path.basename(f)
+            if cmdlet_filename == "index.html":
+                continue
+
+            cmdlet_name, html_ext = os.path.splitext(cmdlet_filename)
+            update_db(db, cur, cmdlet_name, "Cmdlet", "Contents/Resources/Documents/%s/%s" % (module_name, cmdlet_filename))
+
+    # # commit and close db
+    db.commit()
+    db.close()
+
+    # tarball and gunzip the docset
+    make_docset(
+        docset_dir,
+        destination_dir,
+        docset_name
+    )
+    
